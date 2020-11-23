ad029222ed8724e2b6713cc228fe8a604e1a5aff
Jason Juhyun Kim <mtinet@hanmail.net>
2020-11-24 00:19:39 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 
+// 입력받기 인터페이스
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 // [START imports]
 const firebase = require('firebase-admin');
 // [END imports]
@@ -15,59 +22,77 @@ firebase.initializeApp({
 });
 
 const db = firebase.database();
-console.log("firebase is setted up")
+console.log("firebase is setted up...")
+console.log("")
 // [END initialize]
 
-var idNum;
-
-// 자료 읽기
-var ref = db.ref("users");
-ref.on('value', function(snapshot) {
-  var key = snapshot.key;
-  var data = snapshot.val();
-  console.log(typeof key, key);
-  console.log(typeof data, data);
-
-  const ksjs = Object.keys(data);
-  console.log(ksjs);
-
-  for (const [key, value] of Object.entries(data)) {
-  console.log(`${key}:${value.num}:${value.name}:${value.score}`);
-}
-
-//  idNum = data.id;
-//  console.log(typeof idNum, idNum);
+var idNum = 1;
+var key;
+var data;
+var obj_length;
 
 
-//  for(key in data) {
-//    console.log('key:' + key + ' / ' + 'value:' + data[key]);
-//  }
+readDB();
+//writeDB();
 
-/*
-  // object를 json포맷의 문자열로 변환
-  const json = JSON.stringify(data, null, 2);
-  console.log(json);
-
-  // json포맷의 문자열을 array object로 변환
-  const parsed = JSON.parse(json);
-  console.log(typeof parsed, parsed);
-*/
+rl.on("line", function(line) {
+  if (line == "r") {
+    readDB();
+    console.log("read current info");
+    rl.prompt();
+  }
+  if (line == "w") {
+    writeDB();
+    console.log("wrote current info");
+    rl.prompt();
+  }
+  if (line == "exit") {
+    console.log("program exit...");
+    rl.close();
+  }
+}).on("close", function() {
+  process.exit();
 });
 
 
-// 자료 쓰기
-// 기존 자료에 id가 없으면 id라는 ref를 추가 하고 자료를 세팅하고, id가 있으면 그 자료에 자료를 세팅함
-var ref = db.ref('users');
-var aa = {
-  num: 2,
-  name: "345dd",
-  score: 53
+// 자료 읽기
+function readDB() {
+  var ref = db.ref("users");
+  ref.on('value', function(snapshot) {
+    key = snapshot.key;
+    data = snapshot.val();
+    // console.log(typeof key, key);
+    // console.log(typeof data, data);
+    console.log("* 저장되어 있는 데이터 목록");
+    console.log(data);
+
+    obj_length = Object.keys(data).length;
+    console.log("* 저장되어 있는 데이터의 갯수 : " + obj_length);
+
+    idNum = obj_length+1;
+    console.log("* 다음 아이디 번호 : ", idNum);
+
+    console.log("* 'r'을 누르면 기록된 데이터를 불러오고, 'w'를 누르면 데이터를 기록합니다.")
+  });
 }
 
-// push(특정 id를 스스로 만들어서 자료를 올림. 자료는 고유성을 가짐)
-ref.push(aa);
-// set(기존의 자료를 삭제하고 현재 업로드한 자료로 대체함)
-// ref.set(aa);
-// update(기존의 자료는 그대로 두고 현재의 자료를 업데이트 함)
-// ref.update(aa);
-// 등을 사용할 수 있음
+
+// 자료 쓰기
+function writeDB() {
+  // 기존 자료에 id가 없으면 id라는 ref를 추가 하고 자료를 세팅하고, id가 있으면 그 자료에 자료를 세팅함
+  var ref = db.ref("users");
+  var aa = {
+    ["id"+idNum] : {name: "김주현",
+          email: "mtinet@hanmail.net",
+          phone: "010-9483-2362"
+    }
+  }
+  // push(특정 id를 스스로 만들어서 자료를 올림. 자료는 고유성을 가짐)
+  // ref.push(aa);
+  // set(기존의 자료를 삭제하고 현재 업로드한 자료로 대체함)
+  // ref.set(aa);
+  // update(기존의 자료는 그대로 두고 현재의 자료를 업데이트 함)
+  ref.update(aa);
+  console.log("* 'r'을 누르면 기록된 데이터를 불러오고, 'w'를 누르면 데이터를 기록합니다.")
+  // 등을 사용할 수 있음
+}
